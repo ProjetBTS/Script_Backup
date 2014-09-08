be826b915419bd64c6de7b1bee942f870f16e255
Dimitri <dimitri.gdn@gmail.com>
2014-09-08 13:45:21 +0200

    Test v1
    Corps du script pour test

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Ce script est un test a la sauvegarde de fichier par FTP securisé avec SSH
+
+echo "-------------------------------------------------------";
+echo " Sauvegarde de votre machine virtuelle",
+echo "-------------------------------------------------------"; 
+
+echo "Création de l'archive";
+
+# On crée l'archive .tar avec les chemins absolu des dossiers a sauvegarder
+
+
+
